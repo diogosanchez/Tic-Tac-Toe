1584f9248780709cddcbb0ba8aa806e131071b82
Diogo Merguizo Sanchez <sanchezfisica@gmail.com>
2020-08-26 11:17:35 -0300

    Polishing comments.

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -4,7 +4,8 @@ It's a Tic-Tac-Toe game.
 Rules:
 - 2 players should be able to play the game (both sitting at the same computer)
 - The board should be printed out every time a player makes a move
-- You should be able to accept input of the player position and then place a symbol on the board
+- You should be able to accept input of the player position and then place a
+  symbol on the board
 """
 
 
